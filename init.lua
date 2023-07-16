a882287c6dd8ac3175e6554680e5ba422a9e62c1
p-iknow <apricotsoul@gmail.com>
2023-07-16 12:49:49 +0900

    feat: line move 기능 추가,

--- init.lua
+++ init.lua
@@ -216,13 +216,83 @@ function GetBool(message)
 	return bool
 end
 
+-- vscode 에서 visual mode 에서도 alt + up, down 키로 라인무브를 할 수 있는 설정
+-- https://github.com/vscode-neovim/vscode-neovim/issues/200#issuecomment-1245431983
+function MoveVisualSelection(direction)
+	-- vim.pretty_print(vim.fn.line('.'))
+	-- vim.pretty_print(vim.fn.line('v'))
+
+	local cursorLine = vim.fn.line('v')
+	local cursorStartLine = vim.fn.line('.')
+
+	local startLine = cursorLine
+	local endLine = cursorStartLine
+
+	if direction == "Up" then
+		if startLine < endLine then
+			local tmp = startLine
+			startLine = endLine
+			endLine = tmp
+		end
+	else -- == "Down"
+		if startLine > endLine then
+			local tmp = startLine
+			startLine = endLine
+			endLine = tmp
+		end
+	end
+
+	-- move lines
+	vim.cmd("call VSCodeCallRange('editor.action.moveLines" ..
+		direction .. "Action'," .. startLine .. "," .. endLine .. ",1)")
+
+	-- move visual selection
+	if direction == "Up" then
+		if endLine > 1 then
+			startLine = startLine - 1
+			endLine = endLine - 1
+
+			-- exit visual mode
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'x', true)
+
+			-- select range
+			vim.cmd("normal!" .. startLine .. "GV" .. endLine .. "G")
+			-- vim.api.nvim_command(tostring(endLine)) -- move cursor
+			-- vim.api.nvim_feedkeys("V", 'n', false) -- enter visual line mode
+			-- vim.api.nvim_command(tostring(startLine)) -- move cursor
+		end
+	else -- == "Down"
+		if endLine < vim.api.nvim_buf_line_count(0) then
+			startLine = startLine + 1
+			endLine = endLine + 1
+
+			-- exit visual mode
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), 'x', true)
+
+			-- select range
+			vim.cmd("normal!" .. startLine .. "GV" .. endLine .. "G")
+		end
+	end
+end
+
 -- Own Vscode vim setting
 if vim.g.vscode then
 	-- copliot chat
 	local function start_code_chat()
 		vim.fn.VSCodeNotifyVisual("inlineChat.start", true)
 	end
+	-- move line
+	vim.keymap.set('v', '<a-up>', function() MoveVisualSelection('Up') end, { noremap = true })
+	vim.keymap.set('v', '<a-down>', function() MoveVisualSelection('Down') end, { noremap = true })
+
+	vim.keymap.set('v', 'J', function() MoveVisualSelection('Up') end, { noremap = true })
+	vim.keymap.set('v', 'K', function() MoveVisualSelection('Down') end, { noremap = true })
+
 	vim.keymap.set("v", "<D-i>", start_code_chat)
+	-- visual mode mapping to register paste
+
+	vim.keymap.set('v', 'p', '"_dp', { noremap = true, silent = true })
+	vim.keymap.set('v', 'P', '"_dP', { noremap = true, silent = true })
 	-- refactor
 	local function refactor()
 		vim.fn.VSCodeNotifyVisual("editor.action.refactor", true)
