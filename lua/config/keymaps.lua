6540736c90f1bcf6dd92ddd69ca9f02ea442f87e
p-iknow <apricotsoul@gmail.com>
2023-09-25 10:25:56 +0900

    chore: update 및 lint 설정

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-field, undefined-doc-name
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 local Util = require("lazyvim.util")
