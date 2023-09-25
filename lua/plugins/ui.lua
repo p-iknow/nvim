b48c0dd93615e701cafc1f209dd88556b3c03414
p-iknow <apricotsoul@gmail.com>
2023-09-25 10:23:37 +0900

    config: 불필요한 에니메이션 처리 삭제

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,10 +4,6 @@ return {
 	{ "winston0410/range-highlight.nvim", config = true, vscode = true },
 	-- for nvim
 	{
-		"folke/noice.nvim",
-		enabled = false,
-	},
-	{
 		"tummetott/reticle.nvim",
 		config = true,
 		init = function()
@@ -16,29 +12,6 @@ return {
 		end,
 	},
 	{
-		"petertriho/nvim-scrollbar",
-		event = "BufReadPost",
-		opts = {
-			set_highlights = false,
-			excluded_filetypes = {
-				"prompt",
-				"TelescopePrompt",
-				"noice",
-				"neo-tree",
-				"dashboard",
-				"alpha",
-				"lazy",
-				"mason",
-				"DressingInput",
-				"",
-			},
-			handlers = {
-				gitsigns = true,
-			},
-		},
-	},
-
-	{
 		"folke/which-key.nvim",
 		opts = {
 			plugins = {
@@ -46,43 +19,4 @@ return {
 			},
 		},
 	},
-	{
-		"rcarriga/nvim-notify",
-		keys = function(_, keys)
-			return {
-				{ "<leader>un", desc = " Notifications" },
-				{
-					"<leader>und",
-					function()
-						require("notify").dismiss({})
-					end,
-					desc = "Dismiss Notifications",
-				},
-				{
-					"<leader>unl",
-					function()
-						require("telescope").extensions.notify.notify()
-					end,
-					desc = "List Notifications",
-				},
-			}
-		end,
-	},
-	-- fix surround-nvim
-	{
-		"folke/noice.nvim",
-		opts = {
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						kind = "echomsg",
-						find = "No textobject",
-					},
-					opts = { skip = true },
-				},
-			},
-		},
-	},
-
 }
