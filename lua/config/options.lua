5768ea96b2d07899105cc7f43d0652f4ad0b190e
이영창(piknow) <apricotsoul@gmail.com>
2024-09-23 10:45:35 +0900

    fix: neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,13 @@ vim.opt.whichwrap:append("<>hl[]")         -- allow movement between end/start o
 vim.opt.linebreak = true                   -- if text should be wrapped at certain characters
 vim.opt.clipboard:append { 'unnamedplus' } -- copy to system clipboard
 vim.opt.encoding = 'utf-8'
+
+
+if vim.g.vscode then
+else
+    -- -- ------------------------- neovim specific config ----------------------------
+    -- for showing line numbers
+    vim.opt.number = true
+    -- for cursor set in the middle of the screen
+    vim.opt.scrolloff = 15
+end
